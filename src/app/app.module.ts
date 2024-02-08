852e1d6f4a2b2f4abb01addb68458723568fc3c9
shreyash <shreyashparmar1311@gmail.com>
2024-02-08 13:02:02 +0530

    create sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AboutmeComponent
   ],
   imports: [
     BrowserModule,
